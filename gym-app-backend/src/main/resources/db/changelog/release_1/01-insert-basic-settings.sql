c1354a46b54f124d68ba31e221cbf49a99ad7065
ibrahim <=>
2021-05-16 21:13:39 +0200

    add liquibase

---
+++ gym-app-backend/src/main/resources/db/changelog/release_1/01-insert-basic-settings.sql
@@ -0,0 +1,6 @@
+insert
+into settings 
+	(createdAt, lastModifiedAt, code, description, name, value, createdBy_id, lastModifiedBy_id)
+values 
+	(null, null, 'PROFILES_IMAGES_PATH', '', 'Profile Path', 'D:/gym/uploads/profile', null, null)  ,
+	(null, null, 'REPORTS_TEMPLATES_PATH', null, 'Report Path', null, null, null) ;
