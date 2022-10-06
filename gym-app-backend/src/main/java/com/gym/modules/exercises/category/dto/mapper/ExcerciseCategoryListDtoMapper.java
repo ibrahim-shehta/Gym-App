6b346f401cf8cd69b487f68924af62daab8f2ed7
Ibrahim <ibrahim94.tsc@gmail.com>
2022-10-06 19:02:28 +0200

    add dev tools and fix excercies categoreis mapper

--- gym-app-backend/src/main/java/com/gym/modules/exercises/category/dto/mapper/ExcerciseCategoryListDtoMapper.java
+++ gym-app-backend/src/main/java/com/gym/modules/exercises/category/dto/mapper/ExcerciseCategoryListDtoMapper.java
@@ -1,6 +1,7 @@
 package com.gym.modules.exercises.category.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.gym.common.dto.mapper.BaseDtoMapper;
 import com.gym.modules.exercises.category.dto.ExcerciseCategoryListDto;
@@ -9,4 +10,6 @@ import com.gym.modules.exercises.category.model.ExcerciseCategory;
 @Mapper(componentModel = "spring")
 public interface ExcerciseCategoryListDtoMapper extends BaseDtoMapper<ExcerciseCategory, ExcerciseCategoryListDto> {
 
+	@Mapping(target = "nextExcerciseCategoryName", source = "entity.nextExcerciseCategory.name")
+	public abstract ExcerciseCategoryListDto mapEntityToDto(ExcerciseCategory entity);
 }
